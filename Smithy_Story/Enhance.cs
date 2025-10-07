59bcc665478c58ce1fa67112094b4109ea2f692b
Manseong Han <akstjd31@naver.com>
2025-10-08 02:54:02 +0900

    [New] Enhance.cs, EnhanceData.cs
    - Upgrade -> Enhance로 이름 통일(강화)
    - EnhanceData - 강화 확률 리스트 관리
    - Enhance - 성공/실패/파괴

---
+++ Smithy_Story/Enhance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smithy_Story
+{
+    public enum EnhanceResult
+    {
+        Success,    // 성공
+        Fail,       // 실패
+        Destroyed   // 파괴
+    }
+    public class Enhance
+    {
+        // 프로퍼티
+        public double SuccessRate { get; set; }    
+        public double FailRate { get; set; }
+        public double DestroyRate { get; set; }
+
+        // 생성자
+        public Enhance(double successRate, double failRate, double destroyRate)
+        {
+            SuccessRate = successRate; 
+            FailRate = failRate;
+            DestroyRate = destroyRate;
+        }
+    }
+}
---
+++ Smithy_Story/EnhanceData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smithy_Story
+{
+
+    public static class EnhanceData
+    {
+        // <강화 수치, 확률>
+        private static readonly List<Enhance> enhanceList = new List<Enhance>()
+        {
+        new Enhance(1.00, 0.00, 0.00),
+        new Enhance(0.90, 0.10, 0.00),
+        new Enhance(0.75, 0.20, 0.05),
+        new Enhance(0.50, 0.40, 0.10),
+        new Enhance(0.30, 0.50, 0.20)
+        };
+
+        public static Enhance GetData(int level)
+        {
+            if (level >= enhanceList.Count)
+                level = enhanceList.Count - 1;
+
+            return enhanceList[level];
+        }
+    }
+}
